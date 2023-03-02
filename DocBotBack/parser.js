baf8b590c141af1358941b5d36105a69a2ee2727
Jack Wheeler <jack.wheeler@rogers.com>
2023-03-01 23:06:59 -0500

    Parser working for php files

--- DocBotBack/parser.js
+++ DocBotBack/parser.js
@@ -5,7 +5,7 @@ console.log("Parsing text file...")
 const parseTextFile = (fileName) => {
     const data = fs.readFileSync(fileName, {encoding:'utf8', flag:'r'})
     const lines = data.toString().split("\n")
-    return lines.map((line) => line.replace(new RegExp("\t+"), "").replace(new RegExp("\r+"), "")).filter((line) => line.length)
+    return lines.map((line) => line.replace(new RegExp("\t+"), "").replace(new RegExp("\r+"), "").trim()).filter((line) => line.length)
 }
 
 const getAllFunctions = (lines) => {
@@ -14,18 +14,23 @@ const getAllFunctions = (lines) => {
     let bracketCount = 0
     let inFunctionFlag = false
     lines.forEach(line => {
-        if (line.indexOf("function") !== -1 && !(line.indexOf("//") !== -1 && line.indexOf("function") > line.indexOf("//"))){
+        if (line.indexOf("function") === 0){
             inFunctionFlag = true
-            console.log(line)
         }
 
         if (inFunctionFlag) {
+            bracketCount = bracketCount + (line.split("{").length - 1) - (line.split("}").length - 1)
             currentFunction.push(line)
+            if (bracketCount === 0) {
+                inFunctionFlag = false
+                arrOfFunctions.push(currentFunction.join("\n"))
+                currentFunction = []
+            }
         }
     });
     return arrOfFunctions
 }
 
-getAllFunctions(parseTextFile("alphabet_functions.php"))
+console.log(getAllFunctions(parseTextFile("functions.php")))
 
 console.log("Done.")
